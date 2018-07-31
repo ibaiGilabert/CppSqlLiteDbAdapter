bfb811cbf26fe2da19610efceb6c19d06a9d7f9a
Ibai <ibai.gilabert@gmail.com>
2018-07-31 07:50:51 -0700

    Folder reorganization.

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "BinaryValue.h"
-#include <iostream>
-
-int main() {
-  systelab::db::sqlite::BinaryValue binary_value(1);
-
-  std::cout << "Hello World! Just created an unary BinaryValue" << std::endl;
-  return 0;
-}
